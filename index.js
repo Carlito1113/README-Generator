89630d222ab72442c57c815afe6b321e95e3b47b
Carlito Algarin <19lito96@gmail.com>
2020-10-01 20:36:28 -0400

    added in information for the readme file to the generatemarkdownjs

--- index.js
+++ index.js
@@ -11,15 +11,60 @@ inquirer.prompt([
     },
     {
         type:"input",
-        message: "Enter Description",
+        message: "Give a description for this application",
         name: "description"
-    }
+    },
+    {
+        type:"input",
+        message: "How would you install this application?",
+        name: "install"
+    },
+    {
+        type:"input",
+        message: "How do you use this application?",
+        name: "usage"
+    },
+    {
+        type:"input",
+        message: "What licenses are used in this application?",
+        name: "license"
+    },
+    {
+        type:"input",
+        message: "Who contributed to this project?",
+        name: "contributors"
+    },
+    {
+        type:"input",
+        message: "What command is used to run tests?",
+        name: "tests"
+    },
+    {
+        type:"input",
+        message: "Further questions regarding the application",
+        name: "questions"
+    },
+    {
+        type:"input",
+        message: "What is your GitHub username?",
+        name: "github"
+    },
+    {
+        type:"input",
+        message: "What is your email",
+        name: "email"
+    },
 ])
 .then(function(answers) {
     // console.log(answers);
 
-const generatedFile = generateMarkdown(answers)
 
+
+const generatedFile = generateMarkdown(answers);
+
+    fs.writeFile("README.md", markdownFile, function(err){
+        if(err) throw err;
+    })
     console.log(generatedFile);
 
 })
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -7,8 +7,58 @@ function generateMarkdown(answers) {
     ${answers.description}
 
     ### Installation
+    ${answer.install}    
+    
+    ## Table of Contents (Optional)
+    
+    If your README is very long, add a table of contents to make it easy for users to find what they need.
+    
+    * [Installation](#installation)
+    * [Usage](#usage)
+    * [Credits](#credits)
+    * [License](#license)
+    
+    
+    ## Usage 
+    ${answer.usage}
+    Provide instructions and examples for use. Include screenshots as needed. 
+    
+    
+    ## Credits
+    ${answer.contributors}
+    
+    
+    ## License
+    ${answer.license}  
 
+    ## Tests
+    ${answer.tests}  
+    
+    ## questions
+    ${answer.questions}
 
+    ## github user
+    ${answer.github}
 
+    ## email
+    ${answer.email}
+    
+    ---
+    
+    🏆 The sections listed above are the minimum for a good README, but your project will ultimately determine the content of this document. You might also want to consider adding the following sections.
+    
+    ## Badges
+    
+    ![badmath](https://img.shields.io/github/languages/top/nielsenjared/badmath)
+    
+    Badges aren't _necessary_, per se, but they demonstrate street cred. Badges let other developers know that you know what you're doing. Check out the badges hosted by [shields.io](https://shields.io/). You may not understand what they all represent now, but you will in time.
+    
+  
+    ---
+    © 2019 Trilogy Education Services, a 2U, Inc. brand. All Rights Reserved.
+    
+    
     `;
 }
+
+module.exports = generateMarkdown;
