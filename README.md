cefe36a790e8eccd4b8883289fe046a4764b32f3
Carlito Algarin <19lito96@gmail.com>
2020-10-01 22:37:41 -0400

    formatted the index, readme markdown, and generatemarkdown files

--- README.md
+++ README.md
@@ -3,52 +3,59 @@
 [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
 
 ## Description
+
 This is an application that utilizes javascript and node to generate README files
 
 ### Installation
+
 Run this to install
-Run install application    
-    
+Run install application
+
 ## Table of Contents (Optional)
-    
+
 If your README is very long, add a table of contents to make it easy for users to find what they need.
-    
-* [Installation](#installation)
-* [Usage](#usage)
-* [Credits](#credits)
-* [License](#license)
-* [Description](#description)
-* [Contributing](#contributing)
-* [Tests](#tests)
-* [Questions](#questions)
-    
-    
-## Usage 
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
+- [Description](#description)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Usage
+
 This application is used to generate README files for new projects
-Provide instructions and examples for use. Include screenshots as needed. 
-    
-    
+Provide instructions and examples for use. Include screenshots as needed.
+
 ## Credits
+
 Contributors:
 Carlito Algarin
-    
-    
+
 ## License
+
 This application is licensed under
-MIT License  
+MIT License
 
 ## Contributing
+
 After forking this repository, contributions in the form of issues and pull requests are welcomed and encouraged.
 
 ## Tests
+
 Run this command for testing the application
-Test  
-    
+Test
+
 ## Questions
+
 If you have any questions regarding this application contact me at
 my github profile
 Carlito1113
 or my email
 carlito1113@yahoo.com
+
 ## Video Demonstrating Functionality
+
 https://drive.google.com/file/d/1hm1a3aeEkvrkJlzlayULS6a6D21uPRNK/view
--- index.js
+++ index.js
@@ -2,73 +2,74 @@ const inquirer = require("inquirer");
 const fs = require("fs");
 const generateMarkdown = require("./utils/generateMarkdown");
 
-
-inquirer.prompt([
+inquirer
+  .prompt([
     {
-        type:"input",
-        message:"Enter Project Title",
-        name:"title"
+      type: "input",
+      message: "Enter Project Title",
+      name: "title",
     },
     {
-        type:"input",
-        message: "Give a description for this application",
-        name: "description"
+      type: "input",
+      message: "Give a description for this application",
+      name: "description",
     },
     {
-        type:"input",
-        message: "How would you install this application?",
-        name: "install"
+      type: "input",
+      message: "How would you install this application?",
+      name: "install",
     },
     {
-        type:"input",
-        message: "How do you use this application?",
-        name: "usage"
+      type: "input",
+      message: "How do you use this application?",
+      name: "usage",
     },
     {
-        type:"list",
-        choices: ["MIT License", "GNU GPL v3", "Apache 2.0 License"],
-        name: "license"
+      type: "list",
+      choices: ["MIT License", "GNU GPL v3", "Apache 2.0 License"],
+      name: "license",
     },
     {
-        type:"input",
-        message: "Who contributed to this project?",
-        name: "contributors"
+      type: "input",
+      message: "Who contributed to this project?",
+      name: "contributors",
     },
     {
-        type:"input",
-        message: "What command is used to run tests?",
-        name: "tests"
+      type: "input",
+      message: "What command is used to run tests?",
+      name: "tests",
     },
     {
-        type:"input",
-        message: "What is your GitHub username?",
-        name: "github"
+      type: "input",
+      message: "What is your GitHub username?",
+      name: "github",
     },
     {
-        type:"input",
-        message: "What is your email",
-        name: "email"
+      type: "input",
+      message: "What is your email",
+      name: "email",
     },
-])
-.then(function(answers) {
+  ])
+  .then(function (answers) {
     // console.log(answers);
-let badgeURL;
+    let badgeURL;
     if (answers.license === "MIT License") {
-        badgeURL = "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)"
+      badgeURL =
+        "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
     }
     if (answers.license === "GNU GPL v3") {
-        badgeURL = "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)"
+      badgeURL =
+        "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)";
     }
     if (answers.license === "Apache 2.0 License") {
-        badgeURL = "[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)"
+      badgeURL =
+        "[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)";
     }
-    
 
-const generatedFile = generateMarkdown(answers, badgeURL);
+    const generatedFile = generateMarkdown(answers, badgeURL);
 
-    fs.writeFile("README.md", generatedFile, function(err){
-        if(err) throw err;
-    })
+    fs.writeFile("README.md", generatedFile, function (err) {
+      if (err) throw err;
+    });
     console.log(generatedFile);
-
-})
+  });
